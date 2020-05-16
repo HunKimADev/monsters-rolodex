026728a931121dc342ff1a8ff1d91fd210f7b689
HunKimADev <hun.kim@outlook.com>
2020-05-16 08:48:34 +0800

    Updates

---
+++ precache-manifest.b550115279ba2e9e4c639533fabeda12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d4898755d0ee76e981df8e84f6c537",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4dd36edcacb05cc9862f",
+    "url": "/monsters-rolodex/static/css/main.98fb2824.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dd36edcacb05cc9862f",
+    "url": "/monsters-rolodex/static/js/main.ad687809.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
